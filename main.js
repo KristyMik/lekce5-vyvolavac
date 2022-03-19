831167ca344dbdfd621a1a6e7ebbd1703c0d10bc
KristyMik <mikulcova.kristyna@gmail.com>
2022-03-19 21:16:00 +0100

    bonus

--- main.js
+++ main.js
@@ -35,19 +35,41 @@ function tahnoutJmeno() {
     }
 
     // Generujeme náhodný index
-    let vyherniIndex = Math.floor(Math.random()* jmena.length);
+    let vyherniIndex = Math.floor(Math.random() * jmena.length);
+
     // Získáme výherní jméno na patřičném indexu
-    let vyherniJmeno =  jmena[vyherniIndex];
+    let vyherniJmeno = jmena[vyherniIndex];
+
     // Vyřadíme vylosované jméno z osudí
     jmena.splice(vyherniIndex, 1);
+
     // Výherní jméno si uložíme do pole k ostatním výherním
-    tazenaJmena.push(vyherniJmeno); 
+    tazenaJmena.unshift(vyherniJmeno);
 
+    let vyherce = document.querySelector('#vyherka');
+    vyherce.textContent = vyherniJmeno;
 
-let vyherce = document.querySelector('#vyherka');
-vyherce.textContent =  vyherniJmeno;
+    let seznam = document.querySelector('#seznam');
+    seznam.textContent = tazenaJmena;
+    
+    pocitej();
 
-let seznam = document.querySelector('#seznam');
+}
+
+function resetuj() {
+
+tazenaJmena=[];
 seznam.textContent = tazenaJmena;
+pocitadlo.textContent = 'Počet vyvolaných : ' + tazenaJmena.length;
+let vyherce = document.querySelector('#vyherka');
+vyherce.textContent =' ';
+
+}
+
+function pocitej(){
+let pocitadlo = document.querySelector('#pocitadlo');
+
+pocitadlo.textContent = 'Počet vyvolaných : ' + tazenaJmena.length;
+//console.log(pocitadlo);
 
 }
